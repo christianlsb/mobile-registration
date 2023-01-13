71d4ab6a331b611dbc7d2d6bc9c43f182c75983c
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2023-01-13 11:27:52 -0300

    feat: created components in styled-components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
+import { Container } from "./styles/style";
+
 
 function App() {
   return (
     <>
-        <h1>Hello World!</h1>
+        
     </>
   );
 }
---
+++ src/styles/style.js
@@ -0,0 +1,28 @@
+import styled from "styled-components";
+
+
+export const Container = styled.div`
+    
+`
+
+export const ImgPeoples = styled.img`
+
+`
+
+export const Containerinformations = styled.div`
+
+`
+
+export const H1 = styled.h1`
+
+`
+
+export const Label = styled.label`
+
+`
+
+export const Input = styled.input`
+
+`
+
+export const Button = styled.button
