7cc45fae4f00cb40d110652b19dc696415d6bb95
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2023-01-13 13:34:02 -0300

    feat: added function map in application

--- src/App.js
+++ src/App.js
@@ -1,40 +1,47 @@
-import {
-  Container,
-  ImgPeoples,
-  Containerinformations,
-  H1,
-  Label,
-  Input,
-  Button,
-  Arrow,
-} from "./styles/style";
-import PepoleImg from "./assets/imgs/pepole.png";
-import ArrowImg from "./assets/imgs/arrow.png";
+  import {
+    Container,
+    ImgPeoples,
+    Containerinformations,
+    H1,
+    Label,
+    Input,
+    Button,
+    Arrow,
+  } from "./styles/style";
+  import PepoleImg from "./assets/imgs/pepole.png";
+  import ArrowImg from "./assets/imgs/arrow.png";
 
-function App() {
-  const users = [
-    { id: Math.random(), name: "Christian", age: 18 },
-    { id: Math.random(), name: "Marcos", age: 54 },
-  ];
+  function App() {
+    const users = [
+      { id: Math.random(), name: "Christian", age: 18 },
+      { id: Math.random(), name: "Marcos", age: 54 },
+    ];
 
-  return (
-    <>
-      <Container>
-        <ImgPeoples src={PepoleImg} />
-        <Containerinformations>
-          <H1>Olá!</H1>
-          <Label>Nome</Label>
-          <Input />
-          <Label>Idade</Label>
-          <Input />
-          <Button>
-            Cadastrar
-            <Arrow src={ArrowImg} />
-          </Button>
-        </Containerinformations>
-      </Container>
-    </>
-  );
-}
+    return (
+      <>
+        <Container>
+          <ImgPeoples src={PepoleImg} />
+          <Containerinformations>
+            <H1>Olá!</H1>
+            <Label>Nome</Label>
+            <Input />
+            <Label>Idade</Label>
+            <Input />
+            <Button>
+              Cadastrar
+              <Arrow src={ArrowImg} />
+            </Button>
+            <ul align="center">
+              {users.map((user) => (
+                <li key={user.id}>
+                  {user.name} - {user.age}
+                </li>
+              ))}
+            </ul>
+          </Containerinformations>
+        </Container>
+      </>
+    );
+  }
 
-export default App;
+  export default App;
