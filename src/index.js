433a529536ce8c887074a487036c77417d40b535
Christian Lisboa <102741778+christianlsb@users.noreply.github.com>
2023-01-15 19:27:38 -0300

    feat: created themes in project

--- src/index.js
+++ src/index.js
@@ -1,12 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import GlobalStyle from './styles/globalStyle/globalStyle';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import App from "./App";
+import GlobalStyle from "./styles/globalStyle/globalStyle";
+import { themes } from "./styles/theme";
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render(
   <>
-    <App />
-    <GlobalStyle/>
+    <ThemeProvider theme={themes}>
+      <App />
+      <GlobalStyle />
+    </ThemeProvider>
   </>
 );
-
